9696c5e8a2befe81bbd443ed086b910a96ffbccc
Turnote <turnotenaja@gmail.com>
2026-09-18 18:29:27 +0700

    Turnote push ' :

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
-// Simple JavaScript for our portfolio
-
 const menuButton = document.querySelector(".menu-button");
 const navLinks = document.querySelector(".nav-links");
 
-// Mobile menu
 if (menuButton) {
   menuButton.addEventListener("click", function () {
     navLinks.classList.toggle("show");
   });
 }
 
-// Close mobile menu after clicking a link
+
 if (navLinks) {
   const links = navLinks.querySelectorAll("a");
 
@@ -20,5 +17,5 @@ if (navLinks) {
     });
   });
 }
-// Small welcome message when the page loads
-console.log("Welcome to our student portfolio!");
+
+console.log("Welcome to Turnote&Yam portfolio!");
